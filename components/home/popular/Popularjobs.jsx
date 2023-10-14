b8a861473e8442dd0cb7d1d67f4ff695cf205f2b
Ayomide <ayomidedans@gmail.com>
2023-10-14 22:44:38 +0100

    feat: nearby jobs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,66 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
+import { useRouter } from 'expo-router';
 
-import styles from './popularjobs.style'
+import styles from './popularjobs.style';
+import { SIZES, COLORS } from '../../../constants';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import useFetch from '../../../hook/useFetch';
 
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
 
-export default Popularjobs
+  const { data, isLoading, error } = useFetch(
+    'search', {
+    query: 'React Developer',
+    num_pages: 1,
+  }
+  );
+
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = () => {
+
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator color={'orange'} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <PopularJobCard
+                item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
+              />
+            )}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
